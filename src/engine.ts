81469cbebc1f954913fcf5d281ea32939d6c56b6
Francois Best <github@francoisbest.com>
2024-03-18 12:18:08 +0100

    chore: Print time

--- src/engine.ts
+++ src/engine.ts
@@ -1,27 +1,28 @@
-import { db } from "./db/db";
-import { records } from "./db/schema";
-import { fetchNpmStats, getTotalDownloads, sortDownloads } from "./npm";
+import { db } from './db/db'
+import { records } from './db/schema'
+import { fetchNpmStats, getTotalDownloads, sortDownloads } from './npm'
 
 export async function run() {
   const pkgs = await db.query.packages.findMany({
     columns: {
       name: true,
     },
-  });
+  })
   for (const { name: pkg } of pkgs) {
-    await processPackage(pkg);
+    await processPackage(pkg)
   }
 }
 
 async function processPackage(pkg: string) {
-  const tick = performance.now();
-  const downloads = sortDownloads(await fetchNpmStats(pkg));
-  const total = getTotalDownloads(downloads);
+  const tick = performance.now()
+  const downloads = sortDownloads(await fetchNpmStats(pkg))
+  const total = getTotalDownloads(downloads)
   await db.insert(records).values({
     package: pkg,
     versions: downloads,
     total,
-  });
-  const tock = performance.now();
-  console.log(`Processed ${pkg} (${(tock - tick).toFixed(2)}ms)`);
+  })
+  const tock = performance.now()
+  const now = new Date().toISOString()
+  console.log(`${now} Processed ${pkg} (${(tock - tick).toFixed(2)}ms)`)
 }
